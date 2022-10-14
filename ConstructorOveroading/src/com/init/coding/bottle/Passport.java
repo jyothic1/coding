2d661bce9a944148fa6af6961f9d63ad81517756
unknown <jyothic.xworkz@gmail.com>
2022-10-14 09:22:03 +0530

    code madi

--- ConstructorOveroading/src/com/init/coding/bottle/Passport.java
+++ ConstructorOveroading/src/com/init/coding/bottle/Passport.java
@@ -2,5 +2,86 @@ package com.init.coding.bottle;
 
 public class Passport {
 	
+	public long no;
+	public String name;
+	public int expiryDate;
+	public int issueDate;
+	public String country;
+	public int noOfLeafs;
+	
+	public Passport(long no) 
+	{
+		this.no=no;
+		
+	}
+	public Passport(long no,String name ) 
+	{
+		this.no=no;
+		this.name=name;
+		
+	}
+	public Passport(long no,String name,int expiryDate) 
+	{
+		this.no=no;
+		this.name=name;
+		this.expiryDate=expiryDate;
+		
+	}
+	public Passport(long no,String name,int expiryDate,int issueDate) 
+	{
+
+		this.no=no;
+		this.name=name;
+		this.expiryDate=expiryDate;
+		this.issueDate=issueDate;
+		
+	}
+	public Passport(long no,int expiryDate,int issueDate) 
+	{
+		this.no=no;
+		this.expiryDate=expiryDate;
+		this.issueDate=issueDate;
+		
+	}
+	public Passport(int noOfLeafs,long no) 
+	{
+		this.no=no;
+		this.noOfLeafs=noOfLeafs;
+		
+	}
+	public Passport(long no,String name,int expiryDate,int issueDate,int noOfLeafs) 
+	{
+		this.no=no;
+
+		this.name=name;
+		this.expiryDate=expiryDate;
+		this.issueDate=issueDate;
+		this.noOfLeafs=noOfLeafs;
+		
+	}
+	public Passport(int expiryDate,int issueDate,int noOfLeafs) 
+	{
+
+		this.expiryDate=expiryDate;
+		this.issueDate=issueDate;
+		this.noOfLeafs=noOfLeafs;
+		
+	}
+	public Passport(String country,int noOfLeafs) 
+	{
+		this.country=country;
+		this.noOfLeafs=noOfLeafs;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
--- ConstructorOveroading/src/com/init/coding/brush/PassportRunner.java
+++ ConstructorOveroading/src/com/init/coding/brush/PassportRunner.java
@@ -1,10 +1,73 @@
 package com.init.coding.brush;
 
+import com.init.coding.bottle.Passport;
+
 public class PassportRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Passport passport1= new Passport(9845325);
+		 System.out.println(passport1.no);
+		 System.out.println("----------");
+		 
+			Passport passport2= new Passport(9845325,"jyothi");
+			 System.out.println(passport2.no);
+			 System.out.println(passport2.name);
+			 System.out.println("------------------");
+			 
+			 Passport passport3= new Passport(9845325,"jyothi",2030);
+			 System.out.println(passport3.no);
+			 System.out.println(passport3.name);
+			 System.out.println(passport3.expiryDate);
+			 System.out.println("------------------");
+			 
+			 
+			 Passport passport4= new Passport(9845325,"yasmeen",2030,2020);
+			 System.out.println(passport4.no);
+			 System.out.println(passport4.name);
+			 System.out.println(passport4.expiryDate);
+			 System.out.println(passport4.issueDate);
+			 
+			 System.out.println("------------------");
+			 
+			 
+			 Passport passport5= new Passport(9845795,2040,2030);
+			 System.out.println(passport5.no);
+			 System.out.println(passport5.expiryDate);
+			 System.out.println(passport5.issueDate);
+			 
+			 System.out.println("------------------");
+			 
+			 Passport passport6= new Passport(46,2040203);
+			 System.out.println(passport6.no);
+			 System.out.println(passport6.noOfLeafs);
+			 System.out.println("------------------");
+			 
+			 Passport passport7= new Passport(1234567,"vinutha",2030,2019,45);
+			 System.out.println(passport7.no);
+			 System.out.println(passport7.name);
+			 System.out.println(passport7.expiryDate);
+			 System.out.println(passport7.issueDate);
+			 System.out.println(passport7.noOfLeafs);
+			 
+			 
+			 System.out.println("------------------");
+			 
 
+			 Passport passport8= new Passport(2025,2010,50);
+			 
+			 System.out.println(passport8.expiryDate);
+			 System.out.println(passport8.issueDate);
+			 System.out.println(passport8.noOfLeafs);
+			 System.out.println("------------------");
+			 
+			 
+
+Passport passport9= new Passport("India",57);
+			 
+			 System.out.println(passport9.country);
+			 System.out.println(passport9.noOfLeafs);
+			 System.out.println("------------------");
+			
 	}
 
 }
