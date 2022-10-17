5498d2da504d783cb4b84685d899ce40cfa4d3ab
unknown <jyothic.xworkz@gmail.com>
2022-10-17 08:40:43 +0530

    enu illa

--- ConstructorChaining/src/com/practice/chain/chaining/Chain.java
+++ ConstructorChaining/src/com/practice/chain/chaining/Chain.java
@@ -11,9 +11,50 @@ public class Chain {
 	  public boolean fresh;
 	
 	 public Chain(String type){
+		 this.type=type;
 		 
 		 
 	 }
+
+	 public Chain(String type,int price){
+		 this(type);
+		 this.price=price;
+	 }
+
+	 public Chain(int price,String type){
+		 
+		 this(type,price);
+	 }
+
+	 public Chain(String type,String material){
+		 
+		 this.type=type;
+		 this.material=material;
+		 
+	 }
+
+	 public Chain(String type,boolean fresh){
+		 
+		 this.type=type;
+		 this.fresh=fresh;
+	 }
+
+	 public Chain(boolean fresh,String type){
+		 this("horse chain",true);
+		 
+	 }
+
+	 public Chain(double weight,boolean stolen){
+		 
+		 this.weight=weight;
+		 this.stolen=stolen;
+	 }
+
+	 public Chain(boolean stolen,double weight){
+		 this(24.5,false);
+		 
+	 }
+	 
 	 
 	 
 	 
